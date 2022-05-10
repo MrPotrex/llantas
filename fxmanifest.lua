75e0fe573a7b3670d488bf02a8b3bdcb0dd1cd06
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-10 02:27:31 +0200

    Cambiado __resources.lua a fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Desconocido'
-description 'Packs de llantas addon para FiveM'
+description '¡Packs de llantas addon para FiveM!'
 version '3.0'
 
 files {
