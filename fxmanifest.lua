9cc6005eef2ae114bd08c6e25f4f253f15b6f0fd
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-10 02:26:10 +0200

    Cambiado __Resources a FxManifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Desconocido'
+description 'Packs de llantas addon para FiveM'
+version '3.0'
+
+files {
+    'data/*.meta',
+}
+
+data_file 'CARCOLS_FILE'			'data/carcols.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/ganjarimscontentunlocks.meta'
